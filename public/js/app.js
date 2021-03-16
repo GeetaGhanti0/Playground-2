4a955f2de68f64b4af3137ea898768c897c12672
Ghanti <geeta.ghanti@accenture.com>
2021-03-16 13:28:56 +0000

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,48 @@
+//client side Java script 
+
+
+
+
+const weather_form = document.querySelector('form')
+const address_input = document.querySelector('input')
+const p1 = document.querySelector('#message-1')
+const p2 = document.querySelector('#message-2')
+
+p2.textContent =''
+p1.textContent =''
+
+weather_form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    p1.textContent='Loading...'
+    p2.textContent = ''
+
+    const location =  address_input.value
+
+    if(location !== undefined &&  location !== ''){
+
+        fetch('http://localhost:3000/weather?address='+location).then((response) => {
+            console.log(response);
+        response.json().then((data) => {
+            if(!data.error){
+                p1.textContent = data.address 
+                p2.textContent = data.forecast 
+                
+            }
+                else{
+                    p2.textContent = ''
+                    p1.textContent = data.error
+                
+            }
+
+            })
+
+})
+
+} else {
+    p2.textContent = ''
+    p1.textContent = 'Location cannot be empty'
+
+}
+
+})
---
+++ src/app.js
@@ -0,0 +1,113 @@
+const path = require('path')
+
+const express = require('express')
+const hbs = require('hbs')
+const { response } = require('express')
+
+// import util files
+const geocode = require('../../weather-app/utils/geocode')
+const forecast = require('../../weather-app/utils/forecast')
+
+const app = express()
+
+// define paths for Express config
+const publicDirectory = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// setUp static Directory to serve
+app.use(express.static(publicDirectory))
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'weather',
+        name: 'Geeta Ghanti'
+    });
+})
+
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name : 'Geeta Ghanti'
+    });
+})
+
+app.get('/weather', (req, res) => {
+    const address_given = req.query.address
+
+    if(!address_given){
+        return res.send({
+            error: 'you must provide the address'
+        })
+    }
+
+    geocode(address_given, (error, {latitude, longitude, location:address} = {}) => {
+        if(error){
+            return res.send({error})
+        }
+        
+        forecast(latitude,longitude,(error, forecastdata) => {
+            if(error){
+                return res.send(error)
+            }       
+            res.send({
+                location: address_given,
+                forecast: forecastdata, 
+                address
+             })   
+    })
+
+})
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search){
+      return res.send({
+            error: 'you must provide the search product'
+        })    
+    }
+  
+    res.send({
+        product: []
+
+     })
+ })
+ 
+
+app.get('/help', (req, res) => {
+    res.render('help',{
+        title: 'Help',
+        message: 'This is a help message',
+        name : 'Geeta Ghanti'  
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Geeta Ghanti',
+        errorMessage: 'Help article not found'     
+    }) 
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Page not Found',
+        name: 'Geeta Ghanti'
+    }) 
+    })
+
+
+app.listen(3000, () => {
+    console.log('server started  on port 3000')
+})
+
+
